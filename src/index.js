47e0380f8625cd45b32f669be02023be6753ec8f
Gelfande, Tala <tala.gelfande@accenture.com>
2021-02-26 11:31:01 +0200

    fix bug

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/main.scss";
 
-import App from "./App";
+import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
